58a1c06ee60c9694dd30c6d3478484793017555e
Juyong <ht9943@naver.com>
2021-09-27 23:11:33 +0900

    0927

--- Baekjoon/Silver/1789-수들의 합.py
+++ Baekjoon/Silver/1789-수들의 합.py
@@ -7,7 +7,7 @@ while True:
     if result >= S:     # 넘어가거나 같은 순간 바로 break
         break
     else:
-        n += 1
+        n += 1          # 연속되는 자연수
 # print(result)     # 210이 나온다.
 if result > S:      # 200의 예로 들면 이거 다 더하면 210인데 10만 빼면 200이 되는거니까 1개만 빼면 된다.
     print(n-1)
