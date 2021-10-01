252a8a212cf9716d06d6b012bcfaa4ff96b63d52
Juyong <ht9943@naver.com>
2021-10-01 10:49:05 +0900

    1001

---
+++ SWEA/D2/1948-날짜 계산기.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('1948-날짜계산기.txt')
+
+TC = int(input())
+
+for tc in range(1, TC+1):
+    m1, d1, m2, d2 = map(int, input().split())
+
+    # 1/31, 2/28, 3/31, 4/30, 5/31, 6/30,
+    # 7/31, 8/31, 9/30, 10/31, 11/30, 12/31
+
+    days = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
+            7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+
+    day = d2
+
+    if m2 >= m1+2:
+        for x in range(m1+1, m2):
+            day += days[x]
+        day += days[m1]-d1+1
+
+    print('#{} {}'.format(tc, day))
