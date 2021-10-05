56d73cb562ef12ad79efc5f1bd6a5d0dce42419d
Juyong <ht9943@naver.com>
2021-10-05 23:13:55 +0900

    1005

--- Baekjoon/Silver/16922-로마 숫자 만들기.py
+++ Baekjoon/Silver/16922-로마 숫자 만들기.py
@@ -1,14 +1,14 @@
 N = int(input())
 
-I = 1
-V = 5
-X = 10
-L = 50
-
-lst = [1, 5, 10, 50]
-
 result = set()
 
-total = 0
+# 숫자를 안쓰면 0 , 쓰면 1 ~ N
+for a in range(N+1):    # 첫번째 0 ~ N개 쓰고
+    for b in range(N+1-a):  # 두번째 0 ~ N-a개
+        for c in range(N+1-a-b):    # 세번째 0 ~ N-a-b개
+            d = N - a - b - c       # 마지막은 0 ~ N-a-b-c개
+            total = a * 1 + b * 5 + c * 10 + d * 50
+            result.add(total)
+print(len(result))
 
 
