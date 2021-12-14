d2ad06c0b95e774da28ede2dda2fc47d07d693e2
Juyong <ht9943@naver.com>
2021-12-14 14:47:30 +0900

    1214

--- Baekjoon/Silver/1780-종이의 개수.py
+++ Baekjoon/Silver/1780-종이의 개수.py
@@ -1,5 +1,30 @@
 import sys
 sys.stdin = open('종이의 개수.txt')
+sys.setrecursionlimit(10**9)
+def check(i, j, N):
+    global num_minus_one, num_one, num_zero
+    num = arr[i][j]
+    for x in range(i, i+N):
+        for y in range(j, j+N):
+            if num != arr[x][y]:
+                tmp_N = N//3
+                for r in range(0, 3):
+                    for c in range(0, 3):
+                        check(i + r * tmp_N, j + c * tmp_N, tmp_N)
+                return      # return 안쓰면 엉뚱한 답 나온다
+    if num == -1:
+        num_minus_one += 1
+    elif num == 1:
+        num_one += 1
+    else:
+        num_zero += 1
 
 N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+num_minus_one = 0
+num_zero = 0
+num_one = 0
+check(0, 0, N)
+print(num_minus_one)
+print(num_zero)
+print(num_one)
