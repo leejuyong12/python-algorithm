c2446c08c17b868b108d07581faae3c2a0de1a6f
Juyong <ht9943@naver.com>
2022-01-07 14:05:09 +0900

    0107

---
+++ Baekjoon/Silver/1931-회의실 배정.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('회의실배정.txt')
+
+N = int(input())
+lst = [list(map(int, input().split())) for _ in range(N)]
+lst.sort(key=lambda x:(x[1], x[0]))
+
+cnt = 1
+end_time = lst[0][1]
+for x in range(1, N):
+    if lst[x][0] >= end_time:
+        cnt += 1
+        end_time = lst[x][1]
+print(cnt)
