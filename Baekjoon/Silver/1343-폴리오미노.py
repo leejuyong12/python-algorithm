07cdaeb9766ca96927abcf18251aff7d9f597736
Juyong <ht9943@naver.com>
2022-01-12 00:38:58 +0900

    [수정]1343-폴리오미노

--- Baekjoon/Silver/1343-폴리오미노.py
+++ Baekjoon/Silver/1343-폴리오미노.py
@@ -1,7 +1,43 @@
 import sys
 sys.stdin = open('폴리오미노.txt')
 
-board = input().split()
+# board = input()
+#
+# idx = 0
+# while True:
+#     if idx >= len(board):
+#         break
+#
+#     if board[idx:idx+4] == 'XXXX':
+#         idx += 4
+#         board = board.replace('X', 'A', 4)
+#     elif board[idx:idx+2] == 'XX':
+#         idx += 2
+#         board = board.replace('X', 'B', 2)
+#     elif board[idx] == '.':
+#         idx += 1
+#     else:
+#         n = -1
+#         break
+# print(board)
 
-for x in board:
-    if X 
+# board = input()
+# board = board.replace("XXXX", "AAAA")
+# board = board.replace("XX", "BB")
+# if 'X' in board:
+#     print(-1)
+# else:
+#     print(board)
+
+board = input().split('.')
+for i in range(len(board)):
+    if len(board[i]) % 4 == 0:
+        board[i] = 'AAAA' * (len(board[i])//4)
+    elif len(board[i]) % 4 == 2:
+        board[i] = 'AAAA' * (len(board[i])//4) + 'BB'
+res = '.'.join(board)
+if 'X' in res:
+    print(-1)
+
+else:
+    print(res)
