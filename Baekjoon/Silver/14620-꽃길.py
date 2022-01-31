ff0097497a06a755d03358571e0cd694016db479
Juyong <ht9943@naver.com>
2022-02-01 01:15:12 +0900

    [create] 14620-꽃길

--- Baekjoon/Silver/14620-꽃길.py
+++ Baekjoon/Silver/14620-꽃길.py
@@ -1,23 +1,95 @@
 import sys
 sys.stdin = open('꽃길.txt')
 
-dx = [-1, 1, 0 ,0]
-dy = [0, 0, -1, 1]
+# # dy = [0, -1, 1, 0, 0]
+# # dx = [0, 0, 0, -1, 1]
+# dir = [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]
+# def check(y, x):
+#     global N
+#     for dy, dx in dir:
+#         nx = x + dx
+#         ny = y + dy
+#         if 0 > ny or ny > N-1 or 0 > nx or nx > N-1 or visited[ny][nx]:
+#
+#             return False
+#     return True
+#
+# def calculate(y, x):
+#     global N
+#     result = 0
+#     for dy, dx in dir:
+#         nx = x + dx
+#         ny = y + dy
+#         result += flowers[ny][nx]
+#     return result
+#
+# def dfs(a, cost, cnt):
+#     global answer
+#     if cnt == 3:
+#         answer = min(answer, cost)
+#         return
+#     for i in range(a, N):
+#         for j in range(1, N):
+#             if check(i, j):
+#                 visited[i][j] = True
+#                 for dy, dx in dir:
+#                     visited[i+dy][j+dx] = True
+#                 dfs(i, cost+calculate(i, j), cnt+1)
+#                 visited[i][j] = False
+#                 for dy, dx in dir:
+#                     visited[i+dy][j+dx] = False
+#
+#
+#
+# N = int(input())
+# flowers = [list(map(int, input().split())) for _ in range(N)]
+# visited = [[False] * N for _ in range(N)]
+# answer = 99999999
+# dfs(1, 0, 0)
+# print(answer)
 
-def dfs(x, y, N):
-    result = 0
-    visited[x][y] = True
-    for i in range(4):
+dy = [0, -1, 1, 0, 0]
+dx = [0, 0, 0, -1, 1]
+def check(y, x):
+    global N
+    for i in range(5):
         nx = x + dx[i]
         ny = y + dy[i]
-        if (0 <= nx < N and 0 <= ny < N) or visited[nx][ny] == False:
-            result += flowers[nx][ny]
-            return True
-    return False
+        if 0 > ny or ny > N-1 or 0 > nx or nx > N-1 or visited[ny][nx]:
 
+            return False
+    return True
+
+def calculate(y, x):
+    global N
+    result = 0
+    for i in range(5):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        result += flowers[ny][nx]
+    return result
+
+def dfs(a, cost, cnt):
+    global answer
+    if cnt == 3:
+        answer = min(answer, cost)
+        return
+    for i in range(a, N):
+        for j in range(1, N):
+            if check(i, j):
+                visited[i][j] = True
+                for z in range(5):
+                    visited[i+dy[z]][j+dx[z]] = True
+                dfs(i, cost+calculate(i, j), cnt+1)
+                visited[i][j] = False
+                for z in range(5):
+                    visited[i+dy[z]][j+dx[z]] = False
 
 
 N = int(input())
 flowers = [list(map(int, input().split())) for _ in range(N)]
 visited = [[False] * N for _ in range(N)]
-cnt = 0
+answer = 99999999
+dfs(1, 0, 0)
+print(answer)
+
