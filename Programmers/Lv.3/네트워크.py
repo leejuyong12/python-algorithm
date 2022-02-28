19d490669a41d6be9d04badd66674ca50f8198b9
Juyong <ht9943@naver.com>
2022-02-28 23:06:15 +0900

    [create] programmers-네트워크

---
+++ Programmers/Lv.3/네트워크.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+def solution(n, computers):
+    def bfs(i):
+        queue = deque()
+        queue.append(i)
+        while queue:
+            i = queue.popleft()
+            visited[i] = True
+            for a in range(n):
+                if computers[i][a] and visited[a] == False:
+                    queue.append(a)
+
+    answer = 0
+    visited = [False] * n
+    for i in range(n):
+        if visited[i] == False:
+            bfs(i)
+            answer += 1
+
+    return answer
