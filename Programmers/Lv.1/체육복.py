b54f96862b925eceac640108393d12889c219446
Juyong <ht9943@naver.com>
2022-03-09 02:28:40 +0900

    [create] programmers-체육복.py

--- Programmers/Lv.1/체육복.py
+++ Programmers/Lv.1/체육복.py
@@ -1,3 +1,10 @@
 def solution(n, lost, reserve):
-    answer = 0
-    return answer
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
+
+    for x in set_reserve:
+        if x - 1 in set_lost:
+            set_lost.remove(x - 1)
+        elif x + 1 in set_lost:
+            set_lost.remove(x + 1)
+    return n - len(set_lost)
