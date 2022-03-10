cab6dc2b8e3fcf381bf322ddb181bf84747554b6
Juyong <ht9943@naver.com>
2022-03-11 01:39:31 +0900

    [update] programmers-2016년.py

--- Programmers/Lv.1/2016년.py
+++ Programmers/Lv.1/2016년.py
@@ -1,3 +1,10 @@
 def solution(a, b):
-    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    return answer
+    days = [0, 31, 29, 31, 30, 31, 30 , 31, 31, 30, 31, 30, 31]
+    text = ['THU','FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+    day = 0
+    for x in range(1, a):
+        day += days[x]
+    day += b
+    plus = day % 7
+    ans = text[plus]
+    return ans
