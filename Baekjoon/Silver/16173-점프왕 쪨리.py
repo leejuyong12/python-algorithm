126e4e887afa5667bd832dc2ead1ba0dad4f311f
Juyong <ht9943@naver.com>
2023-01-10 00:08:39 +0900

    점프왕 쪨리

---
+++ Baekjoon/Silver/16173-점프왕 쪨리.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open('16173-점프왕 쩰리.txt')
+
+N = int(input())
+graph=[list(map(int,input().split())) for _ in range(N)]
+
+base = [[0] * N for _ in range(N)]
+
+def dfs(x,y):
+
+    if x < 0 or x >= N or y < 0 or y >= N or base[x][y] == 1:
+        return False # return 해도 된다
+    if graph[x][y] == -1: # 끝
+        base[x][y] = 1
+        return False # return 해도 된다
+    base[x][y] = 1 # 방문 기록 남기기
+    dfs(x+graph[x][y], y)
+    dfs(x, y+graph[x][y])
+
+dfs(0,0)
+if base[-1][-1] == 1:
+    print('HaruHaru')
+else:
+    print('Hing')
